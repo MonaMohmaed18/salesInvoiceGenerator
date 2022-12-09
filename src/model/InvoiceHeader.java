18f9727f7c03bf1c8fc6b85fe24fee22ccc05e57
MonaMohmaed18 <mona.abdelrahim18@gmail.com>
2022-12-09 16:10:20 +0200

    intial commit

---
+++ src/model/InvoiceHeader.java
@@ -0,0 +1,6 @@
+package model;
+import java.util.ArrayList; // import the ArrayList class
+
+public class InvoiceHeader {
+    private ArrayList<InvoiceLine> InLine = new ArrayList<InvoiceLine>();
+}
